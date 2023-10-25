044f88ee2976c9a8225844c316724480631f9916
Thiago Alves <thiagoalves.inacio.2013@outlook.com>
2023-10-25 16:35:35 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaFolhaWeb/Pages/FirstPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SistemaFolhaWeb.Pages
+{
+    public class FirstPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SistemaFolhaWeb/Pages/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SistemaFolhaWeb.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
